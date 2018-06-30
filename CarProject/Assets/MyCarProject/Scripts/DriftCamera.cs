66060bdc8e6ab982bdf6604f324acc735c453f8c
Jean-Paul <jpmoise@gmail.com>
2018-06-30 14:28:58 -0400

    Cameras ok
    Pour les prochaines étapes...
    -skidmarks
    -Front Lights voiture devant
    -matériels
    -Reflection probe...

--- CarProject/Assets/MyCarProject/Scripts/DriftCamera.cs
+++ CarProject/Assets/MyCarProject/Scripts/DriftCamera.cs
@@ -1,57 +1,57 @@
 ﻿using System;
 using UnityEngine;
 
-public class DriftCamera : MonoBehaviour {
+public class DriftCamera : MonoBehaviour
+{
+    [Serializable]
+    public class AdvancedOptions
+    {
+        public bool updateCameraInUpdate;
+        public bool updateCameraInFixedUpdate = true;
+        public bool updateCameraInLateUpdate;
+        public KeyCode switchViewKey = KeyCode.Space;
+    }
 
+    public float smoothing = 6f;
+    public Transform lookAtTarget;
+    public Transform positionTarget;
+    public Transform sideView;
+    public AdvancedOptions advancedOptions;
 
-	public class AdvancedOptions
-	{
-		public bool updateCameraInUpdate;
-		public bool updateCameraInFixedUpdate = true;
-		public bool updateCameraInLateUpdate;
-		public KeyCode switchViewKey = KeyCode.Space;
-	}
+    bool m_ShowingSideView;
 
-	public float smoothing = 6f;
-	public Transform lookAtTarget;
-	public Transform positionTarget;
-	public Transform sideView;
-	public AdvancedOptions advancedOptions;
+    private void FixedUpdate ()
+    {
+        if(advancedOptions.updateCameraInFixedUpdate)
+            UpdateCamera ();
+    }
 
-	bool m_ShowingSideView;
+    private void Update ()
+    {
+        if (Input.GetKeyDown (advancedOptions.switchViewKey))
+            m_ShowingSideView = !m_ShowingSideView;
 
-	private void FixedUpdate ()
-	{
-		if(advancedOptions.updateCameraInFixedUpdate)
-			UpdateCamera ();
-	}
+        if(advancedOptions.updateCameraInUpdate)
+            UpdateCamera ();
+    }
 
-	private void Update ()
-	{
-		if (Input.GetKeyDown (advancedOptions.switchViewKey))
-			m_ShowingSideView = !m_ShowingSideView;
+    private void LateUpdate ()
+    {
+        if(advancedOptions.updateCameraInLateUpdate)
+            UpdateCamera ();
+    }
 
-		if(advancedOptions.updateCameraInUpdate)
-			UpdateCamera ();
-	}
-
-	private void LateUpdate ()
-	{
-		if(advancedOptions.updateCameraInLateUpdate)
-			UpdateCamera ();
-	}
-
-	private void UpdateCamera ()
-	{
-		if (m_ShowingSideView)
-		{
-			transform.position = sideView.position;
-			transform.rotation = sideView.rotation;
-		}
-		else
-		{
-			transform.position = Vector3.Lerp(transform.position, positionTarget.position, Time.deltaTime * smoothing);
-			transform.LookAt(lookAtTarget);
-		}
-	}
+    private void UpdateCamera ()
+    {
+        if (m_ShowingSideView)
+        {
+            transform.position = sideView.position;
+            transform.rotation = sideView.rotation;
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, positionTarget.position, Time.deltaTime * smoothing);
+            transform.LookAt(lookAtTarget);
+        }
+    }
 }
