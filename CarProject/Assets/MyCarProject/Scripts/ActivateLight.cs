0396255cf63c99f33a0d798c05aa060ebe7a16ca
Jean-Paul <jpmoise@gmail.com>
2018-07-03 08:21:02 -0400

    changements du 2 juillet 2018

---
+++ CarProject/Assets/MyCarProject/Scripts/ActivateLight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateLight : MonoBehaviour {
+
+    public GameObject RLight;
+    public GameObject LLight;
+    public Transform from;
+    public Transform to;
+    public float turnSpeed = 50f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+         if (Input.GetKey(KeyCode.O))
+          LLight.transform.Rotate(Vector3.right, turnSpeed*Time.deltaTime);
+
+         if (Input.GetKey(KeyCode.O))
+          RLight.transform.Rotate(Vector3.right, turnSpeed * Time.deltaTime);
+
+
+      
+    }
+}
