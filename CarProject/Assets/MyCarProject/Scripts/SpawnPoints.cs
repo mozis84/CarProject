cf4bab6f6aaab90297baffdb02c8a9d224a888ec
Jean-Paul <jpmoise@gmail.com>
2018-08-09 10:32:49 -0400

    Terrain Creation
    Major changements depuis le dernier envoi sur Github + Funnel

---
+++ CarProject/Assets/MyCarProject/Scripts/SpawnPoints.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoints : MonoBehaviour {
+
+
+    public List<GameObject> vehicles;
+    public Transform spawnPoints;
+    private int m_VehicleId;
+
+  
+    void Update () 
+    {
+
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            Transform vehicleTransform = vehicles[m_VehicleId].transform;
+            vehicleTransform.rotation = Quaternion.Euler(0, 90, 0);
+
+            Transform closest = spawnPoints.GetChild(0);
+
+            // Find the closest spawn point.
+            for (int i = 0; i < spawnPoints.childCount; ++i)
+            {
+                Transform thisTransform = spawnPoints.GetChild(i);
+
+                float distanceToClosest = Vector3.Distance(closest.position, vehicleTransform.position);
+                float distanceToThis = Vector3.Distance(thisTransform.position, vehicleTransform.position);
+
+                if (distanceToThis < distanceToClosest)
+                {
+                    closest = thisTransform;
+                }
+            }
+
+            // Spawn at the closest spawn point.
+#if UNITY_EDITOR
+            Debug.Log("Teleporting to " + closest.name);
+#endif
+            vehicleTransform.rotation = closest.rotation;
+
+            // Try refining the spawn point so it's closer to the ground.
+            // Here we assume there is only one renderer.  If not, looping over all the bounds could do the trick.
+            var renderer = vehicleTransform.gameObject.GetComponentInChildren<MeshRenderer>();
+            // A valid car must have at least one wheel.
+            var wheel = vehicleTransform.gameObject.GetComponentInChildren<WheelCollider>(); 
+
+            RaycastHit hit;
+            // Boxcast everything except cars.
+            if (Physics.BoxCast(closest.position, renderer.bounds.extents, Vector3.down, out hit, vehicleTransform.rotation, float.MaxValue, ~(1 << LayerMask.NameToLayer("Car"))) )
+            {
+                vehicleTransform.position = closest.position + Vector3.down * (hit.distance - wheel.radius);
+            }
+            else
+            {
+                Debug.Log("Failed to locate the ground below the spawn point " + closest.name);
+                vehicleTransform.position = closest.position;
+            }
+
+            // Reset the velocity.
+            var vehicleBody = vehicleTransform.gameObject.GetComponent<Rigidbody>();
+            vehicleBody.velocity = Vector3.zero;
+            vehicleBody.angularVelocity = Vector3.zero;
+        }
+    }
+}
