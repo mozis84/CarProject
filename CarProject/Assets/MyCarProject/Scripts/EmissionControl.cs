9cfdcdb159ab1ba1dab7a3ea6bee3d8a248c63d6
Jean-Paul <jpmoise@gmail.com>
2018-09-20 12:40:27 -0400

    preparation pour premiers tests

---
+++ CarProject/Assets/MyCarProject/Scripts/EmissionControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmissionControl : MonoBehaviour {
+
+   public Material material;
+
+	// Use this for initialization
+	void Start () {
+
+
+        
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("i"))
+        {
+            material.EnableKeyword("_EMISSION");
+
+        }
+
+
+        if (Input.GetKeyDown("o"))
+        {
+            material.DisableKeyword("_EMISSION");
+
+        }
+
+    }
+}
---
+++ CarProject/Assets/MyCarProject/Scripts/Switch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("i"))
+        {
+            this.GetComponent<Light>().enabled = true;
+
+        }
+
+
+        if (Input.GetKeyDown("o"))
+        {
+            this.GetComponent<Light>().enabled = false;
+
+        }
+
+    }
+}
